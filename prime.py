34e09c22a50941b80399b4bd1d4f58c380197ed3
originalcheng <chengh@gmail.com>
2018-01-02 18:06:35 -0800

    use dict to sort

--- prime.py
+++ prime.py
@@ -12,13 +12,10 @@ def prime(n):
     return True
 
 def compare(list1, list2):
+    start = datetime.datetime.now()
     res = []
     list1 = sorted(set(list1))
     list2 = sorted(set(list2))
-    if len(list1) > len(list2):
-        list3 = list1
-        list1 = list2
-        list2 = list3
 
     j = 0
     for i in range(len(list1)):
@@ -26,12 +23,27 @@ def compare(list1, list2):
             if list1[i]==list2[j]:
                 res.append(list2[j])
             j= j+1
+    print(datetime.datetime.now()-start)
+    return res
+
+def compare2(list1, list2):
+    start = datetime.datetime.now()
+    dict = {}
+    res=[]
+    for i in range(len(list1)):
+        dict[list1[i]]= True
+    for i in range(len(list2)):
+        if list2[i] in dict.keys():
+            res.append(list2[i])
+    print(datetime.datetime.now()-start)
     return res
 
 if __name__ == "__main__":
-    for i in (1, 2, 25334334331, 133, 211, 121):
-        print(i, prime(i))
+    # for i in (1, 2, 25334334331, 133, 211, 121):
+    #     print(i, prime(i))
 
     list1=[100, 12, 1,2,3,4,5,6,7,8]
     list2=[2,5,8,10,12]
+
     print(compare(list1,list2))
+    print(compare2(list1,list2))
